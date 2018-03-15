4a6543d9cca33807f39e9ebc3bbae9d5bff1eaff
Nikhil Sikka <ns.sikka@gmail.com>
2018-03-16 01:27:33 +0530

    removed gulp-connect in favor of browsersync

--- gulp/server.js
+++ gulp/server.js
@@ -2,10 +2,11 @@ var gulp = require('gulp'),
     connect = require('gulp-connect'),
     livereload = require('gulp-livereload');
 
-gulp.task('connect', function() {
+gulp.task('server', function() {
   connect.server({
     root: './',
     port: 3000,
     livereload: true
   });
 });
+
--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,31 @@
 var gulp = require('gulp'),
     fs = require('fs');
-
-
+    gulp = require('gulp'),
+    browserSync = require('browser-sync').create(),
+    sass = require('gulp-sass');
+    
 /*import files from 'gulp/'' */
 fs.readdirSync(__dirname + '/gulp').forEach(function(task) {
     require('./gulp/' + task)
 })
 
-// build assets for development environment
-gulp.task('build:dev', ['sass:dev', 'inject']);
-
 // enable watch for scripts and styles and create js and style in dist/
 gulp.task('watch', ['watch:style']);
 
-// build assets for productionc
-// gulp.task('build:prod', ['sass', 'script']);
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
 
-// run localhost for development
-gulp.task('serve', ['build:dev', 'connect', 'inject', 'watch']);
+    browserSync.init({
+        server: "./",
+        open: false,
+    });
+
+    gulp.watch("src/scss/**/*.scss", ['sass']);
+    gulp.watch("./index.sample.html").on('change', function() {
+        gulp.start('sass:dev')
+        gulp.start('inject')        
+    });
+    gulp.watch('./index.html').on('change', browserSync.reload);
+});
+
+gulp.task('default', ['serve', 'watch']);
